989943c0e969814c5313cb0bb2150d85bf573dfc
Arkadiusz Drukała <arkadiusz.drukala@gmail.com>
2020-09-11 08:58:47 +0200

    Updates

---
+++ precache-manifest.9e43e9553034a78c90db4b534db60f8b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098cfaf0b703a90a74955a0955bccb2f",
+    "url": "/Dogs-to-adopt/index.html"
+  },
+  {
+    "revision": "755e6d6fa0a83b8bd79c",
+    "url": "/Dogs-to-adopt/static/css/main.0c9d6931.chunk.css"
+  },
+  {
+    "revision": "dcb743b1a0c262eb6f98",
+    "url": "/Dogs-to-adopt/static/js/2.00fb1d88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dogs-to-adopt/static/js/2.00fb1d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755e6d6fa0a83b8bd79c",
+    "url": "/Dogs-to-adopt/static/js/main.4a2097c0.chunk.js"
+  },
+  {
+    "revision": "1da6f7443f64b319cd7f",
+    "url": "/Dogs-to-adopt/static/js/runtime-main.3b34bab4.js"
+  },
+  {
+    "revision": "ef6fa1e42d471ebd508de09c3599bd2f",
+    "url": "/Dogs-to-adopt/static/media/husky.ef6fa1e4.jpg"
+  },
+  {
+    "revision": "d87bbdb195a13c339deebaa87206fa33",
+    "url": "/Dogs-to-adopt/static/media/kundelek.d87bbdb1.jpg"
+  },
+  {
+    "revision": "d045c3a847579dced47d9d61cbfd7ae5",
+    "url": "/Dogs-to-adopt/static/media/kundelek2.d045c3a8.jpg"
+  },
+  {
+    "revision": "6ff8b6da601037ae8dabca9e18418ec9",
+    "url": "/Dogs-to-adopt/static/media/logo.6ff8b6da.jpg"
+  },
+  {
+    "revision": "86a8e304d9f8d8e95ed0a1574dc9f57f",
+    "url": "/Dogs-to-adopt/static/media/miniaturowy-sznaucer.86a8e304.jpg"
+  },
+  {
+    "revision": "99ccb9788b1b786a428ca12f91c917f6",
+    "url": "/Dogs-to-adopt/static/media/owczarek.99ccb978.jpeg"
+  },
+  {
+    "revision": "bc9796828faf80fcf9afbcf00638748f",
+    "url": "/Dogs-to-adopt/static/media/shiba.bc979682.jpg"
+  },
+  {
+    "revision": "8595a6cfc57dfb34616348d632d958e5",
+    "url": "/Dogs-to-adopt/static/media/shiz-tzu.8595a6cf.jpg"
+  },
+  {
+    "revision": "b4f36f4817edd48fb74d63a3f8d5670e",
+    "url": "/Dogs-to-adopt/static/media/spaniel.b4f36f48.jpg"
+  }
+]);
